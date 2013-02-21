76846fb9dcc1b6db279142f22f965f73f1bac8ab
Kriti+Shaun <kriti+shaun@appacademy.io>
2013-02-21 13:40:54 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RiceCream::Application.config.secret_token = '023360a3394f0e78d6b42d61d305bfd251f1ed68e67ac29430c210454cab49cd177ba342a3c8f067145ba43c2432008237eb948b91087324c6ef38c68f5557c8'
